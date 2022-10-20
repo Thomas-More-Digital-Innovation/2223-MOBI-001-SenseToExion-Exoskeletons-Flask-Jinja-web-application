9d588c2ee0f45360f5dbf324ff5444ec134f7d82
thierry-eeman <thierry.eeman@gmail.com>
2022-10-21 00:19:48 +0200

    added Daisy UI config in tailwind.config.js

--- code/tailwind.config.js
+++ code/tailwind.config.js
@@ -10,4 +10,16 @@ module.exports = {
   plugins: [
     require("daisyui")
   ],
+
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
